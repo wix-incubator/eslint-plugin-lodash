5a0c2c0ad23e47c5a6e1f2ce91a2d4edee9e5f23
Omer Ganim <ganimomer@gmail.com>
2015-08-24 11:47:55 +0300

    fix error where prefer-filter reports on if/else

--- tests/lib/rules/prefer-filter.js
+++ tests/lib/rules/prefer-filter.js
@@ -16,6 +16,8 @@ var ruleError = {message: 'Prefer _.filter or _.some over an if statement inside
 ruleTester.run('prefer-filter', rule, {
     valid: [{
         code: 'var x = _.filter(arr, function(x) {return x + 7})'
+    }, {
+        code: '_.forEach(arr, function(x) { if (x) {} else {}})'
     }],
     invalid: [{
         code: '_(arr).forEach(function(x) { if (x) {}})',

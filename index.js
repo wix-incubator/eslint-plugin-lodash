4c0aaea98069013e7f90b928b013388c547e91a3
Omer Ganim <ganimomer@gmail.com>
2015-09-03 17:14:43 +0300

    change index.js to load rules dynamically

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 'use strict';
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var rules = fs.readdirSync(path.resolve('lib', 'rules')).map(function (f) {
+    return f.replace(/\.js$/, '');
+});
 
 module.exports = {
-    rules: {
-        'preferred-alias': require('./lib/rules/preferred-alias'),
-        'prefer-chain': require('./lib/rules/prefer-chain'),
-        'prop-shorthand': require('./lib/rules/prop-shorthand'),
-        'matches-prop-shorthand': require('./lib/rules/matches-prop-shorthand'),
-        'no-single-chain': require('./lib/rules/no-single-chain'),
-        'prefer-reject': require('./lib/rules/prefer-reject'),
-        'prefer-filter': require('./lib/rules/prefer-filter'),
-        'no-unnecessary-bind': require('./lib/rules/no-unnecessary-bind'),
-        unwrap: require('./lib/rules/unwrap'),
-        'prefer-compact': require('./lib/rules/prefer-compact')
-    },
-    rulesConfig: {
-        'preferred-alias': 0,
-        'prefer-chain': 0,
-        'prop-shorthand': 0,
-        'matches-prop-shorthand': 0,
-        'no-single-chain': 0,
-        'prefer-reject': 0,
-        'prefer-filter': 0,
-        'no-unnecessary-bind': 0,
-        unwrap: 0,
-        'prefer-compact': 0
-    }
+    rules: _.zipObject(rules, rules.map(function (rule) {
+        return require('./lib/rules/' + rule);
+    })),
+    rulesConfig: _.zipObject(rules, _.fill(Array(rules.length), 0)) //eslint-disable-line new-cap
 };

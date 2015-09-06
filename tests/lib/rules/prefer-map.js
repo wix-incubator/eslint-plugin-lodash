4bc175c1e7f56e02d6d7814fb14b709f346d3e01
Omer Ganim <ganimomer@gmail.com>
2015-09-06 17:44:27 +0300

    make sure prefer-map is only triggered when callee has only one statement

--- tests/lib/rules/prefer-map.js
+++ tests/lib/rules/prefer-map.js
@@ -18,6 +18,8 @@ ruleTester.run('prefer-map', rule, {
         code: 'var x = _.map(arr, function(x) {return x + 7})'
     }, {
         code: '_.forEach(arr, function(x) { if (x.a) {a.push(x)}})'
+    }, {
+        code: '_.forEach(arr, function (x){ a.push(x); if (f(x)) {a.push(b)}});'
     }],
     invalid: [{
         code: '_(arr).forEach(function(x) { a.push(x)})',

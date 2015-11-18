3c30cad0ae7a5e6a7c8a95e400c09f2ff9942d17
maria <mariao@wix.com>
2015-11-18 16:08:31 +0200

    add matches-shorthand rule and add functions to matches-prop-shorthand

---
+++ docs/rules/matches-shorthand.md
@@ -0,0 +1,24 @@
+# Matches shorthand
+
+When using certain method in lodash such as filter, code can be shorter and more readable when using the `_.matches` callback shorthand. This rule will enforce using shorthand when possible to keep consistency in your code.
+
+## Rule Details
+
+This rule takes no arguments.
+
+The following patterns are considered warnings:
+
+```js
+var result = _.filter(users, function (user) { return user.age === 30 && user.name === 'Bob'; });
+```
+
+The following patterns are not considered warnings:
+
+```js
+var result = _.filter(users, {age: 30, name: 'Bob');
+```
+
+
+## When Not To Use It
+
+If you do not want to enforce `_.matches` callback shorthand, then you can disable this rule.
--- tests/lib/rules/matches-shorthand.js
+++ tests/lib/rules/matches-shorthand.js
@@ -4,7 +4,7 @@
 // Requirements
 // ------------------------------------------------------------------------------
 
-var rule = require('../../../lib/rules/matches-prop-shorthand');
+var rule = require('../../../lib/rules/matches-shorthand');
 var RuleTester = require('eslint').RuleTester;
 
 
@@ -13,12 +13,12 @@ var RuleTester = require('eslint').RuleTester;
 // ------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
-var errors = [{message: 'Prefer matches property syntax'}];
+var errors = [{message: 'Prefer matches syntax'}];
 ruleTester.run('matches-prop-shorthand', rule, {
     valid: [
         'var isPublic = _.find([], function (i) { return x.id; });',
         'var r = _.findIndex(this.packages, {name: name});',
-        'var isPublic = _.map([], function (i) { return i.id === 3; });',
+        'var isPublic = _.map([], function (i) { return i.id + "?"; });',
         'lang.fonts = _.filter(lang.fonts, function (font) { return font.permissions !== "legacy"});',
         'var isPublic = _.findLastIndex([], function (i) { return i.id == 3; });',
         'var isPublic = _.select([], function (i) { return i[0] === 3; });',
@@ -31,12 +31,30 @@ ruleTester.run('matches-prop-shorthand', rule, {
         code: 'var isPublic = _.find([], function (i) { return i.id === 3; });',
         errors: errors
     }, {
+        code: 'var isPublic = _.detect([], function (i) { return i.id === 3 && i.a === "string" && {a: 10} === i.b;});',
+        errors: errors
+    }, {
         code: 'var isPublic = _.filter(arr, i => i.id === 3)',
         ecmaFeatures: {arrowFunctions: true},
         errors: errors
     }, {
-        code: 'var isPublic = _.findIndex(arr, (i) => {return i.id === b.id})',
+        code: 'var isPublic = _.select(arr, i => i.id === 3 && i.a === "string" && {a: 10} === i.b)',
         ecmaFeatures: {arrowFunctions: true},
+        errors: errors
+    }, {
+        code: 'var isPublic = _.findIndex(arr, (i) => {return i.id === 3})',
+        ecmaFeatures: {arrowFunctions: true},
+        errors: errors
+    }, {
+        code: 'var isPublic = _.some(arr, (i) => {return i.id === 3 && i.a === "string" && {a: 10} === i.b})',
+        ecmaFeatures: {arrowFunctions: true},
+        errors: errors
+    }, {
+        code: 'var isPublic = _.every(arr, (i) => {return i.id === b.id})',
+        ecmaFeatures: {arrowFunctions: true},
+        errors: errors
+    }, {
+        code: '_.findLastIndex(arr, function(i) { return i.b.c === compId; });',
         errors: errors
     }]
 });

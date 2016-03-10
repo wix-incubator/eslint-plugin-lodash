ed4279898b40b8f9f12552d89b2ec61aa2ca6c12
Omer Ganim <ganimomer@gmail.com>
2016-03-10 10:37:38 +0200

    fix false positive for prefer-lodash-chain where callback uses lodash (fixes #37)

--- tests/lib/rules/prefer-lodash-chain.js
+++ tests/lib/rules/prefer-lodash-chain.js
@@ -12,25 +12,23 @@ var RuleTester = require('eslint').RuleTester;
 // ------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
-var toErrorObject = require('../testUtil/optionsUtil').fromMessage('Prefer lodash chain');
 ruleTester.run('prefer-lodash-chain', rule, {
     valid: [
-        'var userNames = users.map(function(user) { return user.name; });',
-        'var userNames = _(users).filter({active: true}).map("name").value();',
-        'var userNames = _(users).map("name.givenName").reduce(function (res, cur) { return res + " " + cur; });',
-        'var userNames = users.map(cb1).filter(cb2);',
-        'var userNames = _(users).map(cb1).reduce(cb2).map(cb3);',
-        'var userNames = _(users).map("name.givenName").join(" ");',
         'var userNames = _.map(users, "name.givenName").join(" ");',
-        'var userNames = _(users).filter("active").map("name.givenName").value().toString();'
+        'var userNames = _(users).filter("active").map("name.givenName").value().toString();',
+        {
+            code: 'var minAgePerName = _(users).groupBy("name").map(sameName => _.minBy(sameName, "age")).value()',
+            parserOptions: {ecmaVersion: 6}
+        }
     ],
     invalid: [
-        'var userNames = _.filter(users, cb1).map(cb2);',
-        'var userNames = _(users).map("name.givenName").value().join(" ");',
-        'var userNames = _.filter(users, {active: true}).map(function (user) { return user.name.givenName; });',
-        'var userNames = _(users).map("name.givenName").value().reduce(cb);',
-        'var userNames = _(users).map(cb1).filter(cb2).map(cb3).value().reduce(cb4);',
-        'var userNames = _(users).map("name.givenName").value().reduce(function (res, cur) { return res + " " + cur; });',
-        'var userNames = _.chain(users).filter("active").map("name.givenName").value().reduce(cb);'
-    ].map(toErrorObject)
+        {
+            code: 'var userNames = _.filter(users, cb1).map(cb2);',
+            errors: [{message: "Do not break chain before method 'map'."}]
+        },
+        {
+            code: 'var userNames = _(users).map("name.givenName").value().join(" ");',
+            errors: [{message: "Do not break chain before method 'join'."}]
+        }
+    ]
 });

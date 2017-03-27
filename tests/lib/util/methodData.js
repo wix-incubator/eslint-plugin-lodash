d3ba2cf86128752bd793bf36d900abeb963c19a7
Omer Ganim <ganimomer@gmail.com>
2017-03-27 10:30:40 +0300

    refactor method data test

--- tests/lib/util/methodData.js
+++ tests/lib/util/methodData.js
@@ -3,15 +3,15 @@ const assert = require('assert')
 const v4 = require('../../../src/util/methodDataByVersion/4')
 const _ = require('lodash')
 describe('methodData', () => {
-    it('should have an entry for every item', () => {
+    it('should have an entry for every item on Lodash', () => {
         const functions = _.keys(_.pickBy(_, m => _.isFunction(m) && m !== _))
-        for (const f of functions) {
-            assert(v4[f] || _.some(v4, ({aliases}) => _.includes(aliases, f)), `${f} has no entry`)
-        }
+        const methodsNotCovered = _(functions).difference(_.keys(v4)).reject(f => _.some(v4, ({aliases}) => _.includes(aliases, f))).value()
+        assert.equal(methodsNotCovered.length, 0, `there are no entries for methods ${methodsNotCovered}`)
+    })
+    it('should have an entry for every item on the wrapper object', () => {
         const wrapper = _()
         const wrapperFunctions = _.keys(_.pickBy(wrapper, _.isFunction))
-        for (const f of wrapperFunctions) {
-            assert(v4[f] || _.some(v4, ({aliases}) => _.includes(aliases, f)), `wrapper method ${f} has no entry`)
-        }
+        const methodsNotCovered = _(wrapperFunctions).difference(_.keys(v4)).reject(f => _.some(v4, ({aliases}) => _.includes(aliases, f))).value()
+        assert.deepEqual(methodsNotCovered.length, 0, `there are no entries for wrapper methods ${methodsNotCovered}`)
     })
 })

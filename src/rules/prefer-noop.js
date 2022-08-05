2e8c9ddbf930f306f3b5e973956ab63c70ee04a4
James A Rosen <jamesr@everlane.com>
2022-08-05 16:46:46 -0700

    improve: prefer-noop supports ifContainsComment flag
    Closes #272

--- src/rules/prefer-noop.js
+++ src/rules/prefer-noop.js
@@ -12,7 +12,16 @@ const getDocsUrl = require('../util/getDocsUrl')
 module.exports = {
     meta: {
         type: 'problem',
-        schema: [],
+        schema: [
+            {
+                type: 'object',
+                properties: {
+                    ifContainsComment: {
+                        type: 'boolean'
+                    }
+                }
+            }
+        ],
         docs: {
             url: getDocsUrl('prefer-noop')
         }
@@ -20,11 +29,28 @@ module.exports = {
 
     create(context) {
         const {getFirstFunctionLine} = require('../util/astUtil')
+        const ifContainsComment = context.options[0] && context.options[0].ifContainsComment
+
+        function containsComment(node) {
+            const code = context.getSourceCode()
+            const comments = code.getCommentsInside(node)
+            return comments.length > 0
+        }
 
         function reportIfEmptyFunction(node) {
-            if (!getFirstFunctionLine(node) && node.parent.type !== 'MethodDefinition' && !node.generator && !node.async) {
-                context.report({node, message: 'Prefer _.noop over an empty function'})
+            if (node.parent.type === 'MethodDefinition' || node.generator || node.async) {
+                return
             }
+
+            if (getFirstFunctionLine(node)) {
+                return
+            }
+
+            if (ifContainsComment && containsComment(node)) {
+                return
+            }
+
+            context.report({node, message: 'Prefer _.noop over an empty function'})
         }
 
         return {

011a00ca01ee3dd1e01de527bce4c031e10b10c3
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-11-19 20:20:38 +0100

    fix: added some safeguards for prefer-is-empty-rule

--- src/rules/prefer-is-empty.js
+++ src/rules/prefer-is-empty.js
@@ -35,45 +35,49 @@ module.exports = {
         const visitors = lodashContext.getImportVisitors()
         visitors.BinaryExpression = function (node) {
             if (node.operator === '===') {
-                if (node.left.property) {
-                    const leftExpressionMember = node.left.property.name
-                    const rightExpressionMember = node.right.value
-                    if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                        const subjectObject = node.left.object
-                        context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
-                            return fixer.replaceText(node, `isEmpty(${getTextOfNode(subjectObject)})`)
-                        }})
-                    }
-                } else if (node.left.expression) {
-                    const leftExpressionMember = node.left.expression.property.name
-                    const rightExpressionMember = node.right.value
-                    if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                        const subjectObject = node.left.expression.object
-                        context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
-                            return fixer.replaceText(node, `isEmpty(${getTextOfNode(subjectObject)})`)
-                        }})
+                if (node.left) {                    
+                    if (node.left.property && node.right) {
+                        const leftExpressionMember = node.left.property.name 
+                        const rightExpressionMember = node.right.value
+                        if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
+                            const subjectObject = node.left.object
+                            context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                                return fixer.replaceText(node, `isEmpty(${getTextOfNode(subjectObject)})`)
+                            }})
+                        }                    
+                    } else if (node.left.expression && node.right && node.left.expression.property) {
+                        const leftExpressionMember = node.left.expression.property.name 
+                        const rightExpressionMember = node.right.value
+                        if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
+                            const subjectObject = node.left.expression.object
+                            context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                                return fixer.replaceText(node, `isEmpty(${getTextOfNode(subjectObject)})`)
+                            }})
+                        }              
                     }
                 }
             }
             if (node.operator === '>') {
-                if (node.left.property) {
-                    const leftExpressionMember = node.left.property.name
-                    const rightExpressionMember = node.right.value
-                    if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                        const subjectObject = node.left.object
-                        context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
-                            return fixer.replaceText(node, `!isEmpty(${getTextOfNode(subjectObject)})`)
-                        }})
-                    }
-                } else if (node.left.expression) {
-                    const leftExpressionMember = node.left.expression.property.name
-                    const rightExpressionMember = node.right.value
-                    if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                        const subjectObject = node.left.expression.object
-                        context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
-                            return fixer.replaceText(node, `!isEmpty(${getTextOfNode(subjectObject)})`)
-                        }})
-                    }
+                if (node.left) {
+                    if (node.left.property && node.right) {
+                        const leftExpressionMember = node.left.property.name 
+                        const rightExpressionMember = node.right.value
+                        if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
+                            const subjectObject = node.left.object
+                            context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                                return fixer.replaceText(node, `!isEmpty(${getTextOfNode(subjectObject)})`)
+                            }})
+                        } 
+                    } else if (node.left.expression && node.right) {
+                        const leftExpressionMember = node.left.expression.property.name 
+                        const rightExpressionMember = node.right.value
+                        if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
+                            const subjectObject = node.left.expression.object
+                            context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                                return fixer.replaceText(node, `!isEmpty(${getTextOfNode(subjectObject)})`)
+                            }})
+                        }              
+                    }                    
                 }
             }
         }
